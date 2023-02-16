4506da70f94a14ed9002e9a568a23a807e71c2d4
bhavishya-8 <109825091+bhavishya-8@users.noreply.github.com>
2023-02-16 19:59:05 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1,6 +1,9 @@
-#include<iostream>
-using namespace std;
+// Online C++ compiler to run C++ program online
+#include <iostream>
 
-int main(){
-  cout << "This is for checking Jenkin progress!!";
+int main() {
+    // Write C++ code here
+    std::cout << "Hello world!";
+
+    return 0;
 }
